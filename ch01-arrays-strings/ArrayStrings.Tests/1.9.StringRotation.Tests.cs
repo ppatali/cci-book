64affaee769f9fd468f4798de4ce8bdc53cdb655
Prashanth Patali <patali@gmail.com>
2021-08-12 08:42:48 +0530

    New Samples added

---
+++ ch01-arrays-strings/ArrayStrings.Tests/1.9.StringRotation.Tests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ArrayStrings;
+
+namespace ArrayStrings.Tests
+{
+    public class StringRotationTests
+    {
+        [Fact]
+        public void EqualString()
+        {
+            Assert.True(StringRotation.isRotation("abc", "abc"));
+        }
+
+        [Fact]
+        public void Shift2String()
+        {
+            Assert.True(StringRotation.isRotation("abc", "cab"));
+        }
+
+        [Fact]
+        public void NotRotationEqualString()
+        {
+            Assert.False(StringRotation.isRotation("abc", "cba"));
+        }
+        [Fact]
+        public void SampleFromBook()
+        {
+            Assert.True(StringRotation.isRotation("waterbottle", "erbottlewat"));
+        }
+        [Fact]
+        public void BothEmpty()
+        {
+            Assert.True(StringRotation.isRotation("", ""));
+        }
+        [Fact]
+        public void OneNull()
+        {
+            Assert.False(StringRotation.isRotation(null, "erbottlewat"));
+        }
+        [Fact]
+        public void UnequalLength()
+        {
+            Assert.False(StringRotation.isRotation("waterbottle", "water bottle"));
+        }
+    }
+}
---
+++ ch01-arrays-strings/ArrayStrings/1.9.StringRotation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayStrings
+{
+    public class StringRotation
+    {
+        public static bool isRotation(string s1, string s2)
+        {
+            if (string.IsNullOrEmpty(s1) && string.IsNullOrEmpty(s2))
+                return true;
+            else if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s2))
+                return false;
+            else if (s1 == s2)
+                return true;
+            else if (s1.Length != s2.Length)
+                return false;
+
+            if ((s1 + s1).Contains(s2))
+                return true;
+            else
+                return false;
+        }
+    }
+}
