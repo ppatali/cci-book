dcfb9ee5d8d9682341380790b1bc43650d2b9b3c
Prashanth Patali <patali@gmail.com>
2021-09-05 16:14:12 +0530

    avoid extra instruction

--- ch01-arrays-strings/Q06StringCompression.py
+++ ch01-arrays-strings/Q06StringCompression.py
@@ -6,12 +6,10 @@ def Compress(input: str) -> str:
     
     for i in range(len(input)):
         count += 1
-        if i + 1 < len(input) and input[i] != input[i+1]:
+        if i + 1 == len(input) or input[i] != input[i+1]:
             output.append(input[i] + str(count))
             count = 0
 
-    output.append(input[i] + str(count))
     outputstr = ''.join(output)
 
     return outputstr if len(outputstr) < len(input) else input
-        
